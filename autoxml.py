ec680c4c0eeb17638341985cdb335699294ceb03
raicastroprado <101987467+raicastroprado@users.noreply.github.com>
2023-04-12 13:45:25 -0300

    Update autoxml.py

--- autoxml.py
+++ autoxml.py
@@ -2,7 +2,8 @@
 #codigo para colocar .xml no final de todos os arquivos  ou entao qualquer outro tipo de extensão seja XML, JPEG, PNG, TXT e etc.
 import os
 
-caminho_diretorio = 'C:\\Users\\Fiscal4\\Desktop\\XML SABÓ 06.01.23 A 22.01.23' # caminho onde esta os arquivos para alterar lero lero
+#sempre que colocar o caminho usar // na separação 
+caminho_diretorio = ' coloque aqui o caminha desejado ' # caminho onde esta os arquivos para alterar lero lero
 for nome_arquivo in os.listdir(caminho_diretorio):
     if not nome_arquivo.endswith('.xml'):
         novo_nome_arquivo = nome_arquivo + '.xml'
